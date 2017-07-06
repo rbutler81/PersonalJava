a470a9a9dade9ccc0c0d7fb8a6ceb42f5bf0a464
RyanB <RyanB@192.168.1.7>
2017-07-06 13:51:38 -0400

    added minimum amount value to cointype enum

--- HelperLibraries/src/helpers/econ/currency/CoinType.java
+++ HelperLibraries/src/helpers/econ/currency/CoinType.java
@@ -2,7 +2,7 @@ package helpers.econ.currency;
 
 public enum CoinType {
 	
-	BTC(8, "0.00000001", "BTC", "btc", "0.0005"), ETH(8, "0.00000001", "ETH", "eth", "0.00000100"), 
+	BTC(8, "0.00000001", "BTC", "btc", "0.0005"), ETH(8, "0.00000001", "ETH", "eth", "0.00001000"), 
 	CAD(2, "0.01", "CAD", "cad", "0.01"), USD(2, "0.01", "USD", "usd", "0.01");
 	
 	private int decimalPlaces;

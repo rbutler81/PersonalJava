f6f5583e1d4d9a63175f991061c589aebb1bb092
RyanB <RyanB@RyanB-M4800.rmt.otl>
2017-10-05 14:53:42 -0400

    removed "init" method

--- ALMDEditor/src/com/cimcorp/plcUtil/almdEditor/CheckBoxEnhanced.java
+++ ALMDEditor/src/com/cimcorp/plcUtil/almdEditor/CheckBoxEnhanced.java
@@ -4,33 +4,25 @@ import javafx.scene.control.CheckBox;
 
 public class CheckBoxEnhanced {
 
-	
-	public static void init(CheckBox chk){
-		
-		
-	}
-	
-	public static void reset(CheckBox chk){
+	public static void reset(CheckBox chk) {
 		chk.setIndeterminate(false);
 		chk.setSelected(false);
 		chk.setDisable(true);
 	}
-	
-	public static void setState(CheckBox chk, int state){
-		
+
+	public static void setState(CheckBox chk, int state) {
+
 		chk.setDisable(false);
-		
+
 		if (state == 1) {
 			chk.setAllowIndeterminate(false);
 			chk.setSelected(true);
 			chk.setIndeterminate(false);
-		}
-		else if (state == 2) {
+		} else if (state == 2) {
 			chk.setAllowIndeterminate(false);
 			chk.setSelected(false);
 			chk.setIndeterminate(false);
-		}
-		else {
+		} else {
 			chk.setAllowIndeterminate(true);
 			chk.setSelected(false);
 			chk.setIndeterminate(true);
